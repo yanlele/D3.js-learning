e7487fea47e61db3354dc91a805f1e1643645d0b
yanlele <331393627@qq.com>
2018-10-15 22:04:09 +0800

    [+]基于到d3.v3 的一个hello world

--- app/01、D3基础：选择集与数据/00、HelloWorld/HelloWorld.ts
+++ app/01、D3基础：选择集与数据/00、HelloWorld/HelloWorld.ts
@@ -3,4 +3,23 @@
  * connect me 331393627@qq.com
  * create time 2018-10-15 21:26
  */
-console.log(123);
+
+import Test from './test.js';
+let test:Test = new Test();
+
+let d3 = (<any>window).d3;
+
+// let paragraphs = document.getElementsByTagName('p');
+// for (let i = 0;i < paragraphs.length; i ++) {
+//     let paragraph = paragraphs[i];
+//     paragraph.innerHTML = 'hello world';
+// }
+
+let p = d3.select('body').selectAll('p').text('hello, world');
+p.style('color', 'red');
+p.style('font-size', '72px');
+
+
+
+
+export default {};
---
+++ app/01、D3基础：选择集与数据/00、HelloWorld/test.ts
@@ -0,0 +1,14 @@
+/**
+ * create by yanle
+ * connect me 331393627@qq.com
+ * create time 2018-10-15 21:49
+ */
+
+class Test {
+    constructor() {
+        console.log('hello test')
+    }
+}
+
+
+export default Test;
