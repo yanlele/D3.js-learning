99804847205e795afd171862775de1ae01a0d2e3
yanlele <331393627@qq.com>
2018-10-18 22:02:11 +0800

    [+]添加线性比例尺的使用实例

--- app/02、比例尺与坐标轴/README.md
+++ app/02、比例尺与坐标轴/README.md
@@ -29,4 +29,27 @@ linear.nice([count]) | 可以将定义域的范围自动扩展为较为理想的
 linear.ticks([count]) | 设定或者获取比较有代表性的值的数目。默认为10， 主要获取坐标轴的刻度。
 linear.tickFormat(count [, format]) | 设定定义域内具有代表性的值的表示形式。比如显示到小数点后两位，适用百分比例行使现实数据。
 
+使用实例：
+```typescript
+import {scaleLinear} from "d3-scale";   
+let linear = scaleLinear().domain([0, 20]).range([0, 100]);
+console.log(linear(10));            // 获取定义域为10 的值域 结果为 50
+console.log(linear(30));                      // 结果150
+console.log(linear.invert(80));         // 结果 16
+
+// 如果不希望超出范围
+linear.clamp(true);
+console.log(linear(30));                        // 100
+
+// 如果希望获取值域的时候四舍五入, 用下面的方法代替range()　方法
+linear.rangeRound([0, 100]);
+console.log(linear(13.33));                    // 67
+
+// 理想化定义域
+linear.domain([0.12300000, 0.4888888]).nice();
+console.log(linear.domain());                  // [0.1, 0.5]
+```
+
+
+
 
--- app/02、比例尺与坐标轴/index.ts
+++ app/02、比例尺与坐标轴/index.ts
@@ -1,17 +1,38 @@
 import {scaleLinear} from "d3-scale";
+import {line} from "d3-shape";
 
 class Index {
-    /*线性比例尺最标准的用法*/
+    /*比例尺*/
     demo1() {
         let linear = scaleLinear().domain([0, 500]).range([0, 100]);
         console.log(linear(50));            // 结果10
         console.log(linear(250));           // 结果50
         console.log(linear(450));           // 结果90
     }
+
+    /*线性比例尺的用法*/
+    demo2() {
+        let linear = scaleLinear().domain([0, 20]).range([0, 100]);
+        console.log(linear(10));            // 获取定义域为10 的值域 结果为 50
+        console.log(linear(30));                      // 结果150
+        console.log(linear.invert(80));         // 结果 16
+
+        // 如果不希望超出范围
+        linear.clamp(true);
+        console.log(linear(30));                        // 100
+
+        // 如果希望获取值域的时候四舍五入, 用下面的方法代替range()　方法
+        linear.rangeRound([0, 100]);
+        console.log(linear(13.33));                    // 67
+
+        // 理想化定义域
+        linear.domain([0.12300000, 0.4888888]).nice();
+        console.log(linear.domain());                  // [0.1, 0.5]
+    }
 }
 
 let index: Index = new Index();
-index.demo1();
+index.demo2();
 
 
 export default Index;
