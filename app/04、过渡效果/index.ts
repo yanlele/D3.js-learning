3e40323a797479e17b8f7471545db416a960fce2
yanlele <331393627@qq.com>
2018-10-23 20:44:48 +0800

    [+]创建过渡

--- app/04、过渡效果/index.ts
+++ app/04、过渡效果/index.ts
@@ -3,12 +3,15 @@
  * connect me 331393627@qq.com
  * create time 2018-10-22 23:03
  */
+import {selection} from "d3-selection";
 
 class Index {
-
+    demo1() {
+    }
 }
 
-
+let index = new Index();
+index.demo1();
 
 
 export default Index;
