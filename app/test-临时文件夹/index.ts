518deb2c0a2c46528bd3866b0d270b4a64d518a0
yanlele <331393627@qq.com>
2018-10-24 19:46:07 +0800

    [+]删除案例

--- app/test-临时文件夹/index.ts
+++ app/test-临时文件夹/index.ts
@@ -23,42 +23,7 @@ class Index {
     }
 
     main() {
-        let rectStep: number = 35;
-        let rectWidth: number = 30;
-        let dataSet: number[] = [50, 43, 120, 87, 99, 167, 142];                // 高度
-        let rect = this.svg.selectAll('rect');
-        rect.data(dataSet)
-            .enter()
-            .append('rect')
-            .attr('width', rectWidth)
-            .attr('height', (d: number) => {
-                return d;
-            })
-            .attr('y', (d: number) => {
-                return this.height - d - this.padding.bottom
-            })
-            .attr('x', (d: number, i: number) => {
-                return this.padding.left + i * rectStep;
-            })
-            .attr('fill', schemeCategory10[0]);
 
-        let text = this.svg.selectAll('text');
-        text.data(dataSet)
-            .enter()
-            .append('text')
-            .attr('dx', rectWidth / 2)
-            .attr('dy', '1em')
-            .attr('x', (d: number, i: number) => {
-                return this.padding.left + i * rectStep
-            })
-            .attr('y', (d: number, i: number) => {
-                return this.height - this.padding.bottom - d;
-            })
-            .attr('text-anchor', 'middle')
-            .attr('fill', schemeCategory10[1])
-            .text((d:number) => {
-                return d
-            })
     }
 
 
