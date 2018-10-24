c982ce552b28463e33157f2ddfabe6c3b4d40df8
yanlele <331393627@qq.com>
2018-10-24 19:24:19 +0800

    [+]测试用的文件

---
+++ app/test-临时文件夹/index.ts
@@ -0,0 +1,17 @@
+/**
+ * create by yanlele
+ * create time 2018-10-24 19:23
+ */
+
+class Index {
+
+    main() {
+
+    }
+}
+
+let index:Index = new Index();
+index.main();
+
+
+export default Index;
