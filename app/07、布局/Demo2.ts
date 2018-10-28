c03cd5b1bf9542564c96543ea762949cd4586ce1
yanlele <331393627@qq.com>
2018-10-28 09:37:50 +0800

    [+]提交定义函数

--- app/07、布局/Demo2.ts
+++ app/07、布局/Demo2.ts
@@ -7,7 +7,10 @@ import {event, select, selectAll} from "d3-selection";
 import {scaleOrdinal} from "d3-scale";
 import {merge, range} from "d3-array";
 import {schemeCategory10} from "d3-scale-chromatic";
-import {forceCenter, forceLink, forceManyBody, forceSimulation} from "d3-force";
+import {
+    forceCenter, forceLink, forceManyBody, forceSimulation, SimulationLinkDatum,
+    SimulationNodeDatum
+} from "d3-force";
 import {drag} from "d3-drag";
 
 class Demo2 {
@@ -18,8 +21,8 @@ class Demo2 {
         right: 60
     };
     private svg;
-    private width = 600;
-    private height = 600;
+    private width = 800;
+    private height = 800;
 
     private nodes: { name: string }[] = [
         {name: "湖南邵阳"},
@@ -57,125 +60,24 @@ class Demo2 {
     }
 
     main() {
-        function ticked() {
-            links
-                .attr('x1', function (d) {
-                    return d.source.x
-                })
-                .attr('y1', function (d) {
-                    return d.source.y
-                })
-                .attr('x2', function (d) {
-                    return d.target.x
-                })
-                .attr('y2', function (d) {
-                    return d.target.y
-                });
-
-            linkText
-                .attr('x', function (d: any) {
-                    return (d.source.x + d.target.x) / 2;
-                })
-                .attr('y', function (d: any) {
-                    return (d.source.y + d.target.y) / 2;
-                });
-
-            gs.attr('transform', function (d: any) {
-                return `translate(${d.x}, ${d.y})`
-            })
-        }
-
-        //　设置一个颜色比例尺
-        let colorScale = scaleOrdinal().domain(range(this.nodes.length).map(function (d: number) {
-            return d.toString()
+        // 颜色比例尺
+        let colorScale = scaleOrdinal().domain(range(this.nodes.length).map(function (item) {
+            return item.toString();
         })).range(schemeCategory10);
 
-        // 周边数据
+        // 生成边集数据
         let forceLinkMain = forceLink().links(this.edges).distance(function (d: any) {
-            return d.value * 100
+            return d.value * 100;
         });
 
-        // 设置圆形的中间位置
+        // 设置图形中心位置
         let forceCenterMain = forceCenter().x(this.width / 2).y(this.height / 2);
 
-        // 创建一个力导向
         let forceSimulationMain = forceSimulation()
-            .nodes(this.nodes)                                      // 生成节点数据
-            .on('tick', ticked)                          // 非常重要
+            .nodes(this.nodes)
             .force('link', forceLinkMain)
-            .force('change', forceManyBody())
-            .force('center', forceCenterMain);
-
-        // 这个时候 可以看到, 把我们的基础数据抛给 上面函数运行一遍，我们的基础函数就就得到了编译了
-        console.log(this.nodes);
-        console.log(this.edges);
-
-        // 绘制边
-        let links = this.g.append('g')
-            .selectAll('line')
-            .data(this.edges)
-            .enter()
-            .append('link')
-            .attr('stroke', function (d: any, i: number) {
-                return colorScale(i.toString())
-            });
-
-        // 绘制文字
-        let linkText = this.g.append('g')
-            .selectAll('text')
-            .data(this.edges)
-            .enter()
-            .append('text')
-            .text(function (d) {
-                return d.relation
-            });
-
-        // 多拽函数
-        let dragFun = drag().on('start', function (d: any) {
-            if (!event.action) {
-                forceSimulationMain.alphaTarget(0.8).restart();
-            }
-            d.fx = d.x;
-            d.fy = d.y;
-        }).on('end', function (d: any) {
-            if (!event.active) {
-                forceSimulationMain.alphaTarget(0);
-            }
-            d.fx = null;
-            d.fy = null;
-        }).on('drag', function (d: any) {
-            d.fx = event.x;
-            d.fy = event.y;
-        });
-
-        // 建立用来放在每个节点和对应文字的分组<g>
-        let gs = selectAll('.circleText')
-            .data(this.nodes)
-            .enter()
-            .append('g')
-            .attr('transform', function (d: any, i: number) {
-                let cirX = d.x;
-                let cirY = d.y;
-                return `translate(${cirX}, ${cirY})`
-            });
-
-
-        // 节点和文字
-        gs.append('circle')
-            .attr('r', 10)
-            .attr('fill', function (d: any, i: number): any {
-                return colorScale(i.toString())
-            });
-
-        gs.append('text')
-            .attr('x', -10)
-            .attr('y', -20)
-            .attr('dy', 10)
-            .text(function (d: any) {
-                return d.name
-            });
-
-
+            .force('charge', forceManyBody())
+            .force('center', forceCenterMain)
     }
 }
 
