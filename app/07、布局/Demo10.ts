94be5c3567a6fb1778ef8b9e5eb3322d070e44f7
yanlele <331393627@qq.com>
2018-10-31 23:46:07 +0800

    [+]添加矩阵图

--- app/07、布局/Demo10.ts
+++ app/07、布局/Demo10.ts
@@ -4,9 +4,98 @@
  * create time 2018-10-31 21:25
  */
 
+
+/*基础示例*/
+import {select} from "d3-selection";
+import {hierarchy, treemap, treemapResquarify} from "d3-hierarchy";
+import {schemeCategory10} from "d3-scale-chromatic";
+
 class Demo10 {
     main() {
+        let height: number = 600;
+        let width: number = 1200;
+        let svg = select('body')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height);
 
+        let data:any = {
+            "name":"gen",
+            "children":[
+                {
+                    "name":"num1",
+                    "children":[{"name":"num1-1","size":"5"},{"name":"num1-2","size":"5"}]
+                },
+                {
+                    "name":"num2",
+                    "size":10
+                },
+                {
+                    "name":"num3",
+                    "size":10
+                },
+                {
+                    "name":"num4",
+                    "size":10
+                },
+                {
+                    "name":"num5",
+                    "size":10
+                }
+            ]
+        };
+
+        let treeMapMain = treemap()
+            .tile(treemapResquarify)
+            .size([width, height])
+            .round(true)
+            .padding(10);
+
+        let hi = hierarchy(data)
+            .sum(function (d: any) {
+                return d.size
+            })
+            .sort(function (a, b) {
+                return b.value-a.value
+            });
+
+        treeMapMain(hi);
+
+        let cell = svg.selectAll('g')
+            .data(hi.leaves())
+            .enter()
+            .append('g');
+
+        cell.append('rect')
+            .attr('x', function (d: any) {
+                return d.x0
+            })
+            .attr('y', function (d: any) {
+                return d.y0
+            })
+            .attr('height', function (d: any) {
+                return d.y1 - d.y0
+            })
+            .attr('width', function (d: any) {
+                return d.x1 - d.x0
+            })
+            .attr('fill', schemeCategory10[0]);
+
+        cell.append('text')
+            .attr('x', function (d: any) {
+                return d.x0
+            })
+            .attr('y', function (d: any) {
+                return d.y0
+            })
+            .attr('dy', '1.5em')
+            .attr('dx', '1em')
+            .attr('font-size', 18)
+            .attr('stroke', 'white')
+            .attr('fill', 'none')
+            .text(function (d: any) {
+                return `name: ${d.data.name} - size: ${d.data.size}`
+            })
     }
 }
 
